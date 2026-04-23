151052a619e9598b8705ea45a0f4d2049d1ab0e5
Aftab007-lab <kamalshaaftab@gmail.com>
2026-04-23 23:37:06 +0530

    Update install_httpd.sh
    Script is ready

--- scripts/install_httpd.sh
+++ scripts/install_httpd.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+# Update system package (-y for yes)
+yum update -y
+
+# Install Apache HTTP Server
+yum install httpd -y
+
+# Start and enable service
+systemctl start httpd
+systemctl enable httpd
+
+# Create simple web page
+echo "Hello this is from $(hostname)" > /var/www/html/index.html
+
+# Setting proper permissions
+chmod 755 /var/www/html/index.html
